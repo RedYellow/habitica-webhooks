599b64dd76ed834dccdfcefec610817bbb183a5b
Ryan <ryan.mai@mail.com>
2020-11-04 14:28:46 -0800

    trying to fix the template rendering

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import psycopg2
 
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
