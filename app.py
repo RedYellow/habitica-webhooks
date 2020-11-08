b86204f1e75c86964d6016ea0821f6b47577291c
Ryan <ryan.mai@mail.com>
2020-11-08 10:54:55 -0800

    minor fixes, cleanup

--- app.py
+++ app.py
@@ -19,14 +19,15 @@ from sqlalchemy.dialects.postgresql import JSON
 
 import json
 
-from config import Config #TODO: do I need this?
+# from config import Config #TODO: do I need this?
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True # for prettyprinting in /getall
 db = SQLAlchemy(app)
 
-app.config.from_object(Config()) #TODO: remove?
+# app.config.from_object(Config()) #TODO: remove?
 
 try:
     str_tz = os.environ["TZ"] #TODO: make this into an app.config['str_tz'] = ...
@@ -61,36 +62,13 @@ db.init_app(app)
 def home():    
     records=Record.query.all()
     js = [e.serialize_json() for e in records]
-    data = []
-    for i in js:
-        # print(i)
-        inf = js_extract(i)
-        # print(inf)
-        data.append(inf)
-        # if "data" in i["data"].keys():
-        #     # local mistakes
-        #     data.append(js_extract(i["data"]))
-        # else:
-        #     # this is what it should be (in heroku)
-        #     data.append(js_extract(i))
-        
-        # try:
-        #     # this is for the local
-        #     i = i["data"]
-        #     data.append(js_extract(i))
-        # except:
-        #     print("zoop")
-        #     # this is for heroku
-        #     data.append(js_extract(i)) 
-    print(data)
+    data = [js_extract(entry) for entry in js ]
             
     return render_template("home.html", data=data)
 
 
 @app.route('/webhook', methods=['POST'])
 def respond():
-    # print("zoop")
-    # print(request)
     rec = Record(data = request.json)
     db.session.add(rec)
     db.session.commit()
@@ -99,7 +77,6 @@ def respond():
 
 @app.route("/getall")
 def get_all():
-    #TODO: app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True # for prettyprinting
     try:
         records=Record.query.all()
         return  jsonify([e.serialize_json() for e in records])
@@ -113,23 +90,20 @@ def tstamp_to_str(timestamp):
 def js_extract(js_entry):
         try:
             if "timestamp" in js_entry["data"].keys():
-                print("data fix")
                 js_entry = js_entry["data"]
                 
-            if js_entry["data"]["type"] == "scored":
-                print("zop")
+            if js_entry["data"]["type"] in ["created", "updated", "deleted", "scored", "checklistScored"]:
                 return [js_entry["timestamp"], 
                         tstamp_to_str(js_entry["timestamp"]), 
                         js_entry["data"]["type"], 
                         js_entry["data"]["task"]["text"]]
-            elif js_entry["data"]["type"] == "petHatched":
-                print("hatched")
+            
+            elif js_entry["data"]["type"] in ["petHatched", "mountRaised", "leveledUp"]:
                 return [js_entry["timestamp"], 
                         tstamp_to_str(js_entry["timestamp"]), 
                         js_entry["data"]["type"], 
                         js_entry["data"]["message"]]
         except Exception as e:
-            print(js_entry)
             print(e)
             return [js_entry["timestamp"], 
                         tstamp_to_str(js_entry["timestamp"]), 
